30f287748ce8a7a53d14fb1618b48d0ed938fb48
k-eks <kreks@gmx.at>
2016-05-06 10:41:04 +0200

    updated in-line docu for better understanding and more userfriendly output

--- hamilton.py
+++ hamilton.py
@@ -3,10 +3,18 @@ import math
 
 
 def R_test(alpha, reflections, wR_A, parameters_A, wR_B, parameters_B):
-    """After Hamilton 1965, used eq 24 to get R values from F values."""
-    """reflections ... n"""
-    """dimension ... b or parameters_B - parameters_A"""
-    """freedom ... b or N1"""
+    """Performs a Hamilton-R Test for a structure, method described below.
+    !!!Model A MUST be the model with fewer parameters!!!
+    alpha ... float percentage value (i.e. 0.05 for 5 %) for the probablility of error
+    reflections ... number of unique reflections (data in shelx) of the measurement
+    wR_A ... float value or percent of R1 value from Model A
+    parameters_A ... int number of parameters used in Model A
+    wR_B ... float value or percent of R1 value from Model B
+    parameters_B ... int number of parameters used in Model B
+    After Hamilton 1965, used eq 24 to get R values from F values. Input for equation:
+    reflections ... n
+    dimension ... b or parameters_B - parameters_A
+    freedom ... b or N1"""
     dimension = parameters_B - parameters_A
     freedom = reflections - parameters_B
     F = distributions.f.ppf(1 - alpha, dimension, freedom)
@@ -15,4 +23,6 @@ def R_test(alpha, reflections, wR_A, parameters_A, wR_B, parameters_B):
     Rratio = wR_A / wR_B
     print("Rratio = %f\nR = %f" %(Rratio, R))
     print("Hypothesis: Model A is better than Model B")
-    print("Reject if %f > %f at a probablility level of %f: %s" % (Rratio, R, alpha, (Rratio > R)))
+    print("Reject if %f > %f at a probablility level of %f" % (Rratio, R, alpha))
+    print("Result: Hypothesis is %s" % (Rratio < R))
+    # the use of larger than and smaller than signs is correct! It's not a typo, just programing logic
